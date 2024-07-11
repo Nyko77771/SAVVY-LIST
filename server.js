5c49ee57325a66276cf4cccfc773a131484a14a8
Nykyta <60669163+Nyko77771@users.noreply.github.com>
2024-07-11 14:29:38 +0100

    'CSS Changes'

---
+++ server.js
@@ -0,0 +1,67 @@
+//VERSION 1
+//Method sending static HTTP pages
+
+// const http = require("http");
+// // const fs = require("fs");
+// // const port = 8080;
+// // const localhost = "127.0.0.1";
+
+// const server = http.createServer((req, res) => {
+//   let path = "./";
+//   console.log("Server seeing " + req.url);
+//   switch (req.url) {
+//     case "/":
+//       path += "index.html";
+//       res.statusCode = 200;
+//       break;
+//     case "/login":
+//       path += "login.html";
+//       res.statusCode = 200;
+//       break;
+//     case "/register":
+//       path += "registration.html";
+//       res.statusCode = 200;
+//       break;
+//     default:
+//       path += "default.html";
+//       res.statusCode = 404;
+//       break;
+//   }
+//   fs.readFile(path, (error, data) => {
+//     if (error) {
+//       console.log("Error " + error + " has occurred.");
+//       res.statusCode = 500;
+//       res.end("Server Error");
+//     } else {
+//       res.setHeader("Content-Type", "text/html");
+//       res.end(data);
+//     }
+//   });
+// });
+
+// server.listen(port, localhost, (error) => {
+//   if (error) {
+//     console.log("Error " + error + " has occured");
+//   } else {
+//     console.log("Server is waiting");
+//   }
+// });
+
+//VERSION 2
+//Using Express to send Dynamic HTTP objects
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 8080;
+const localhost = "127.0.0.1";
+
+app.use(express.static(path.join(__dirname, "public")));
+console.log(`Serving static files from: ${publicPath}`);
+
+app.listen(port, (error) => {
+  if (error) {
+    console.log(`Error: ${error} has occured`);
+  } else {
+    console.log("Server is listening");
+  }
+});
