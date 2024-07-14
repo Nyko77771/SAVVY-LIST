5b81e7f48648e784e8f6613a021b6598e7c6bb14
Nyko77771 <nykytamcdonald@gmail.com>
2024-07-14 17:42:41 +0100

    'server changes 2'

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@
 
 // const server = http.createServer((req, res) => {
 //   let filePath = "";
-//   console.log("Server seeing " + req.url);
+//   console.log(req.url);
 //   switch (req.url) {
 //     case "/":
 //       filePath += "index.html";
@@ -28,10 +28,12 @@
 //       res.statusCode = 404;
 //       break;
 //   }
+//   console.log("The path currently is:" + filePath);
+//   console.log("Dir name is:" + __dirname);
+//   const absolutePath = path.join(__dirname, "..", "public", filePath);
+//   console.log("Absolute path is:" + absolutePath);
 
-//   const absolutePath = path.join(__dirname, filePath);
-
-//   fs.readFile(absolutePathath, (error, data) => {
+//   fs.readFile(absolutePath, (error, data) => {
 //     if (error) {
 //       console.log("Error " + error + " has occurred.");
 //       res.statusCode = 500;
@@ -53,25 +55,37 @@
 
 //VERSION 2
 //Using Express to send Dynamic HTTP objects
-// const express = require("express");
-// const path = require("path");
-// const app = express();
-// const port = 8080;
-// const localhost = "127.0.0.1";
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 8080;
+const localhost = "127.0.0.1";
 
-// app.use(express.static(path.join(__dirname, "..", "public")));
-// console.log(`Serving static files from: ${localhost}`);
+app.use(express.static(path.join(__dirname, "..", "public")));
+console.log(`Serving static files from: ${localhost}`);
 
-// app.use((req, res, next) => {
-//   const filePath = path.join(__dirname, "..", "public", "404.html");
-//   console.log("File path log:" + filePath);
-//   res.status(404).sendFile(filePath);
-// });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
-// app.listen(port, (error) => {
-//   if (error) {
-//     console.log(`Error: ${error} has occured`);
-//   } else {
-//     console.log("Server is listening");
-//   }
-// });
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "registration.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "login.html"));
+});
+
+app.use((req, res, next) => {
+  const filePath = path.join(__dirname, "..", "public", "404.html");
+  console.log("File path log:" + filePath);
+  res.status(404).sendFile(filePath);
+});
+
+app.listen(port, (error) => {
+  if (error) {
+    console.log(`Error: ${error} has occured`);
+  } else {
+    console.log("Server is listening");
+  }
+});
