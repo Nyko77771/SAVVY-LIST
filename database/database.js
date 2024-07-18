5d2f7e3be45a863d5633230814194d75a656cbfe
Nykyta <60669163+Nyko77771@users.noreply.github.com>
2024-07-18 12:55:43 +0100

    Add files via upload
    Added New Database

--- database/database.js
+++ database/database.js
@@ -1,24 +1,49 @@
 //Using mysql
 const mysql = require("mysql2");
+require("dotenv").config();
+
+mysql.conn;
 
 // MySQL - Database 1
+const pool = mysql
+  .createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: "VeryLongANDB0r1nGAns1",
+    database: process.env.MYSQL_DATABASE,
+  })
+  .promise();
+
+// MySQL - Database 2
 // const pool = mysql
-// host: MYSQL.HOST,
-// user: MYSQL.USER,
-// password: MYSQL.PASSWORD,
-// database: MYSQL.DATABASE,
+//   .createPool({
+//     host: MYSQL2_HOST,
+//     user: MYSQL2_USER,
+//     password: MYSQL2_PASSWORD,
+//     database: MYSQL2_DATABASE,
 //   })
 //   .promise();
 
-// MySQL - Database 2
-const pool = mysql
-  .createPool({
-    host: MYSQL2.HOST,
-    user: MYSQL2.USER,
-    password: MYSQL2.PASSWORD,
-    database: MYSQL2.DATABASE,
-  })
-  .promise();
+// async function testConnection() {
+//   try {
+//     const connection = await pool.getConnection();
+//     console.log("Database has connected successfully");
+//     await connection.query("Select 1");
+//     connection.release();
+//   } catch (error) {
+//     console.log(`An ${error} has occured`);
+//   }
+// }
+
+// testConnection();
+
+async function checkLogin(username, password) {
+  const databaseName = await pool.query(
+    "SELECT user_name FROM user_details WHERE (user_name = ?) AND (user_pass = ?)",
+    [username, password]
+  );
+  username === databaseName ? true : false;
+}
 
 // async function getQuery() {
 //   const result = await pool.query("SELECT * FROM user_details");
@@ -26,6 +51,7 @@ const pool = mysql
 // }
 
 async function insertQuery(username, password, address, email) {
+  console.log(process.env.MYSQL_HOST);
   await pool.query(
     "INSERT INTO user_details (user_name, pass_word, address, email) VALUES (?, ?, ?, ?)",
     [username, password, address, email]
@@ -33,5 +59,6 @@ async function insertQuery(username, password, address, email) {
 }
 
 module.exports = {
+  checkLogin,
   insertQuery,
 };
