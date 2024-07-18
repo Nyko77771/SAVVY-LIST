79faed105322d7dcc042b99c2b984c4f65efd687
Nykyta <60669163+Nyko77771@users.noreply.github.com>
2024-07-18 17:08:16 +0100

    Add files via upload
    Server changes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 app.set("view engine", "ejs");
 require("dotenv").config();
 
-const { insertQuery } = require("./database/database");
+const container = require("./database/database");
 const bodyParser = require("body-parser");
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
@@ -50,26 +50,51 @@ app.get("/registration", (req, res) => {
   res.render("registration");
 });
 
-app.post("/views/registration", urlencodedParser, (req, res) => {
-  console.log(req.body);
+app.post("/views/registration", urlencodedParser, async (req, res) => {
   const { username, password, address, email } = req.body;
+  //Use the first password added
   const userPassword = password[0];
+  //Combine address if two fields were used
   let address1 = req.body.address1;
   let address2 = req.body.address2;
   address2 !== ""
     ? (fullAddress = address1 + " " + address2)
     : (fullAddress = address1);
-  try {
-    insertQuery(username, userPassword, fullAddress, email);
-    console.log("Registration successful");
-    res.redirect("/login");
-  } catch (error) {
-    console.log(`An error ${error} has occured`);
+  console.log("The username is:" + username, "The password is:" + userPassword);
+  //Check if user exists
+  const existingUser = await container.checkLogin(username, userPassword);
+  if (existingUser) {
+    res.send("User exists already. Please choose a different username");
     res.redirect("/register");
+  } else {
+    // Register User
+    try {
+      await container.insertQuery(username, userPassword, fullAddress, email);
+      console.log("Registration successful");
+      res.redirect("/login");
+    } catch (error) {
+      console.log(`An error ${error} has occured`);
+      res.redirect("/register");
+    }
   }
 });
 
-// app.post();
+app.post("/views/login", urlencodedParser, async (req, res) => {
+  const { username, password } = req.body;
+  console.log("username is: " + username + " password is: " + password);
+  try {
+    const checkUser = await container.checkLogin(username, password);
+    if (checkUser) {
+      console.log("Successfully logged in");
+      res.redirect("/");
+    } else {
+      res.redirect("/login");
+    }
+  } catch (error) {
+    console.log("Login unsuccessful");
+    res.redirect("/login");
+  }
+});
 
 app.use((req, res, next) => {
   res.status(404).render("404");
