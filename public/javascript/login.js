b55897dbb43e8cfb6745c8af462276fdeea692ee
Nykyta <60669163+Nyko77771@users.noreply.github.com>
2024-07-27 15:43:46 +0100

    Add files via upload
    Final update 4

---
+++ public/javascript/login.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var loginForm = document.getElementById("loginForm");
+  const loginBtn = document.getElementById("loginBtn");
+  var loginMessage = document.getElementById("loginHidden");
+
+  loginBtn.addEventListener("click", async (e) => {
+    e.preventDefault();
+    loginMessage.innerText = "";
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if (username === "" || password === "") {
+      alert("Please enter details");
+      return false;
+    } else {
+      await testLogin(username, password);
+    }
+  });
+
+  async function testLogin(username, password) {
+    try {
+      const userInfo = {
+        username: username,
+        password: password,
+      };
+      const myJSON = JSON.stringify(userInfo);
+      console.log(myJSON);
+      const response = await fetch("views/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: myJSON,
+      });
+      const data = await response.json();
+
+      console.log(data);
+      if (data.redirect) {
+        window.location.href = data.redirect;
+      } else {
+        loginMessage.innerText = data.text;
+      }
+    } catch (error) {
+      console.log(`Error ${error} occured while checking Login.`);
+    }
+  }
+});
